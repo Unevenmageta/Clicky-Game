d75e2dca28b34688a429663b10ce51f7f88c6ec6
Kayro Figueroa <kayrofigueroa@gmail.com>
2020-02-11 11:55:13 -0700

    Updates

---
+++ precache-manifest.04c54632edb299946eb684487e7da43a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0573d2fd6aa16d7057dd979cd1b1c59d",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "59fe858ff47c45d9c811",
+    "url": "/Clicky-Game/static/css/main.38e43cd3.chunk.css"
+  },
+  {
+    "revision": "75b623ccae057f2b4795",
+    "url": "/Clicky-Game/static/js/2.678eb951.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Clicky-Game/static/js/2.678eb951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59fe858ff47c45d9c811",
+    "url": "/Clicky-Game/static/js/main.f8c6440d.chunk.js"
+  },
+  {
+    "revision": "d181d3898eaebe650f01",
+    "url": "/Clicky-Game/static/js/runtime-main.a1fe3040.js"
+  }
+]);
